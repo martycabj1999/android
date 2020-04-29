02e3c7249ce7ef3ec2f855fb40744270eb2cbd89
martycabj1999 <martingira1999@gmail.com>
2020-04-28 23:48:03 -0300

    comienzo android

---
+++ app/src/main/java/com/platzi/conf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.platzi.conf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.platzi.conf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
